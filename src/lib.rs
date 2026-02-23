979eba294c080cb6648215bda8f1d86f3688439c
quam1123 <quam1123l@gmail.com>
2026-02-23 17:13:29 +0800

    增加子线程信息回收消息通道

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,5 @@
 use std::{
-    net::{Ipv4Addr,SocketAddr,TcpStream},
-    thread,
-    sync::{mpsc,Arc,Mutex},
-    time::Duration
+    net::{Ipv4Addr,SocketAddr,TcpStream}, sync::{Arc, Mutex, mpsc::{self, Sender}}, thread, time::Duration
 };
 #[derive(Debug)]
 pub struct Config{
@@ -17,9 +14,9 @@ impl Config{
     pub fn build(args:Vec<String>) -> Config{
         let ip =args[1].parse().expect("无效的 IP 地址格式");
         
-        let begin_p = args[2].parse().unwrap();
-        let end_p = args[3].parse().unwrap();
-        let thread_num = args[4].parse().unwrap();
+        let begin_p = args[2].parse().expect("起始端口必须是 0-65535 的数字");
+        let end_p = args[3].parse().expect("结束端口必须是 0-65535 的数字");
+        let thread_num = args[4].parse().expect("线程数必须是有效的正整数");
 
         Config { ip, begin_p, end_p, thread_num }
     }
@@ -42,7 +39,7 @@ pub struct ThreadPool {
     workers: Vec<Worker>,
 }
 impl ThreadPool{
-    pub fn new(size:usize) -> ThreadPool{
+    pub fn new(size:usize,result_sender:Sender<u16>) -> ThreadPool{
         assert!(size > 0);
         assert!(size < 101);
         
@@ -53,7 +50,7 @@ impl ThreadPool{
         let mut workers : Vec<Worker> = Vec::with_capacity(size);
 
         for id in 0..size{
-            workers.push(Worker::new(id,Arc::clone(&receiver)));
+            workers.push(Worker::new(id,Arc::clone(&receiver),result_sender.clone()));
         }
         ThreadPool { sender : Some(sender), workers }
     }
@@ -66,7 +63,7 @@ impl Drop for ThreadPool{
     fn drop(&mut self){
         drop(self.sender.take());
         for worker in &mut self.workers{
-            println!("Shutdown Worker{}",worker.id);
+            //println!("Shutdown Worker{}",worker.id);
 
             if let Some(thread) = worker.thread.take(){
                 thread.join().unwrap();
@@ -91,27 +88,28 @@ struct Worker{
 }
 
 impl Worker{
-    fn new(id:usize,receiver : Arc<Mutex<mpsc::Receiver<WorkPackage>>>) -> Worker{
+    fn new(id:usize,receiver : Arc<Mutex<mpsc::Receiver<WorkPackage>>>,result_sender:Sender<u16>) -> Worker{
         let thread = thread::spawn(move|| loop{
             let workpackage = receiver.lock().unwrap().recv();
 
             match workpackage{
                 Ok(workpackage) => {
                     
-                    println!("Worker {id} , Receiver addr {}  Working",workpackage.addr);
+                    //println!("Worker {id} , Receiver addr {}  Working",workpackage.addr);
                     let timeout = Duration::from_secs(3);
 
                     match TcpStream::connect_timeout(&workpackage.addr, timeout){
                         Ok(_stream) => {
-                            println!("Port {} connected successfully",workpackage.addr.port());
+                            //println!("Port {} connected successfully",workpackage.addr.port());
+                            result_sender.send(workpackage.addr.port()).unwrap();
                         }
-                        Err(e) => {
-                            println!("Port {} connected Failed for reason {}",workpackage.addr.port(),e);
+                        Err(_e) => {
+                            //println!("Port {} connected Failed for reason {}",workpackage.addr.port(),e);
                         }
                     }
                 }
                 Err(_) => {
-                    println!("睡觉睡觉睡");
+                    //println!("睡觉睡觉睡");
                     break;
                 }
             }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,31 @@
-use std::{env};
+use std::{env, sync::mpsc};
 use port_scan::{Config, ThreadPool, WorkPackage};
 
 fn main(){
 
     let args : Vec<String> = env::args().collect();
     let config = Config::build(args);
-    let pool = ThreadPool::new(config.get_size());
+
+    let (result_sender , result_receiver) = mpsc::channel();
+     
+    let pool = ThreadPool::new(config.get_size(),result_sender);
 
     for port in config.get_start()..=config.get_end()  {
         let workpackage : WorkPackage = WorkPackage::new(config.get_ip(), port);
         pool.execute(workpackage);
+    }
+
+    drop(pool);
+
+    let mut open_ports : Vec<u16> = Vec::new();
+
+    for i in result_receiver{
+        open_ports.push(i);
+    }
+
+    open_ports.sort();
+    for port in open_ports {
+        println!("port {} is open",port);
     }
 }
 
